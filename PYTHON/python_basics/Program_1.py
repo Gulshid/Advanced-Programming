f21fa6de12faf4b5c07505704d918134c51e0f1f
Gulshid Zada <gulshidzada4@gmail.com>
2025-09-06 13:34:06 +0500

    display value, input value form user, find the type of datatype

---
+++ PYTHON/python_basics/Program_1.py
@@ -0,0 +1,34 @@
+# 1 .How to display the value 
+x='String value'
+print(x)
+
+# 2. How to input from user at run time 
+intger_value=int(input('Enter the int value: '))
+String_value=str(input('Enter the String value :'))
+Boolean_value=bool(input("Enter the boolean value "))
+Float_value=float(input("Enter the decimal/float value:"))
+set_value=set(input('Enter the set value:'))
+list_value=list(input('Enter the list value:'))
+tuple_value=tuple(input('Enter the tuple value:'))
+dict_value=eval(input('Enter the dict value:'))
+
+# 3. How to declare the variable or value
+print('Intger value:', intger_value),
+print('String value:', String_value),
+print('boolean value:', Boolean_value),
+print('flaot value :', Float_value),
+print('set value:', set_value),
+print('List value:', list_value),
+print('Tuple value of x:', tuple_value),
+print('dict value of x:', dict_value),
+
+
+# 4. How to find the datatype of variable
+print('Type is :', type(intger_value)),
+print('Type is :', type(String_value)),
+print('Type is :', type(Boolean_value)),
+print('Type is :', type(Float_value)),
+print('Type is :', type(set_value)),
+print('Type is :', type(list_value)),
+print('Type is :', type(tuple_value)),
+print('Type is :', type(dict_value)),
