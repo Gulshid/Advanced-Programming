3aedf4aeca2bfb8c2b9904fc55a1435ef23936b5
Gulshid Zada <gulshidzada4@gmail.com>
2025-09-07 22:59:33 +0500

    problem solving in task 2

--- PYTHON/python_basics/basics_task/task_2.py
+++ PYTHON/python_basics/basics_task/task_2.py
@@ -11,3 +11,42 @@ heigth=5.10
 is_student=True
 print(name + "'s Favorite Color is :", favorite_color)
 print(f"They are {heigth:.2f} feet tall")
+print(f"Are they a student? {is_student}")
+
+
+# Define variables
+name="Alice"
+age=25
+city='New York'
+Score=95.5
+is_student=True
+print(name, age, city, Score, is_student)
+
+# Define variables
+name='Bob'
+age=30
+profession='Engineer'
+print("Name "+name + ",Age "+ str(age) + ",Profession "+ profession)
+
+
+#Define variables
+product='Laptop'
+price=999.99
+quantity=2
+total= price * quantity
+print(f"Product {product} , proce {price} , quantity {quantity}, Total {total}")
+
+
+# Define variables
+name='Charlie'
+grade= 'A'
+persentage=90.5
+print("name {}, grade {}, persentage {}%".format(name, grade, persentage))
+
+# Define variables
+temperature=72.5
+humidity=65
+weather='sunny'
+print(f"Temperature {temperature}°F,  Humidity {humidity}% ,Condition {weather}")
+
+
