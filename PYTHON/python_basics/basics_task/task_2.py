9bb87ff972ce757aeaef32d16b9dfe9b0c1eff82
Gulshid Zada <gulshidzada4@gmail.com>
2025-09-07 22:14:12 +0500

    Task 2 Started--->

---
+++ PYTHON/python_basics/basics_task/task_2.py
@@ -0,0 +1,13 @@
+# Simple greeting program
+name=input("What's your Name:")
+age=int(input("What's your Age:"))
+print(f' My Name is : {name} and my Age is : {age}')
+print(f'Next year {name} you will be ', age+1, "year old\n")
+
+
+# Working with different data types
+favorite_color='blue'
+heigth=5.10
+is_student=True
+print(name + "'s Favorite Color is :", favorite_color)
+print(f"They are {heigth:.2f} feet tall")
