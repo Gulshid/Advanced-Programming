c78059af19a88e973e41e11ee3583375f5705338
Gulshid Zada <gulshidzada4@gmail.com>
2025-10-01 09:51:08 +0500

    Continue using for and while loop quiz done

---
+++ PYTHON/Quizes/quiz_4.py
@@ -0,0 +1,15 @@
+for number in range(0, 21):
+    if(number % 2 == 0):
+        continue
+    print(number)
+
+
+a = 0
+while a <= 10:
+    if a % 2 == 0:   
+        a += 1      
+        continue     
+    print(a)        
+    a += 1          
+
+        
