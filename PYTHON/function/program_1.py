ae2387775eb2ffbb4f28f7e3773a305fc773803b
Gulshid Zada <gulshidzada4@gmail.com>
2025-10-08 09:41:29 +0500

    7,8,9 program done --->

--- PYTHON/function/program_1.py
+++ PYTHON/function/program_1.py
@@ -5,14 +5,14 @@ def greet():
 
 # calling the function
 greet()
-
+print("*****************")
 # 2nd program
 def greet_person(person):
     print(f"Hello : {person}")
 
 # calling the function
 greet_person("Gulshid")
-
+print("*****************")
 
 # 3rd program
 def introduce(name, age, city):
@@ -21,7 +21,7 @@ def introduce(name, age, city):
 # calling the function
 introduce("Gulshid Zada", 22, "Peshawar")
 
-
+print("*****************")
 # 4th Function with Return Value
 def add_number(a,b):
     result = a+b
@@ -31,7 +31,7 @@ sum = add_number(2,3)
 print(f"The sumof two value is :{sum}")
 print(f"The sum of two value is :{add_number(3,6)}")
 
-
+print("*****************")
 
 # 5th calculate two value 
 def calculate(x,y):
@@ -48,10 +48,66 @@ print(f"The subtraction of two value :{sub}")
 print(f"The div of two value is :{div}")
 print(f"The Mul of two value is :{mul}")
 print(f'calculate result : {calculate_result}')
-
+print("*****************")
 # 6th  Function with Default Parameters
+def create_greeting(name, greeting="Hello", punctuation="!"):   
+    return f"{greeting}, {name}{punctuation}"
+
+print(create_greeting("Alice")) 
+print(create_greeting("Bob", "Hi")) 
+print(create_greeting("Charlie", "Hey", "!!!"))
+print("*****************")
+# 7th Function with Keyword Arguments
+def describe_pet(pet_name, animal_type = "dog", age = 1):
+    print(f"i have {animal_type} named {pet_name} who is  {age} Year old ")
+
+
+describe_pet("whister", "Cat", 3)
+describe_pet("hamster", "nibble", 1)
+describe_pet("whister", "Cat", 2)
+describe_pet("whister", "Cat", 1)
+describe_pet("Buddy") # use defalt value for argument
+print("*****************")
+# 8th. Practical Example: Calculator Functions
+def Calculator():
+    # add function
+    def add(x,y):
+        return x + y
+    
+    # Sub functionn
+    def sub(x,y):
+        return x - y
+    
+    # Mul function
+    def mul(x,y):
+        return x * y
+    
+    # divide function
+    def div(x,y):
+        if(y == 0):
+            return "Error : Division by Zero"
+        return x / y
+    num_1 = 20
+    num_2 = 5
+    print(f"The additon of two value {num_1} and {num_2} is : {add(num_1, num_2)}")
+    print(f"The Subtraction of two value {num_1} and {num_2} is : {sub(num_1, num_2)}")
+    print(f"The Multiplication of two value {num_1} and {num_2} is : {mul(num_1, num_2)}")
+    print(f"The Division of two value {num_1} and {num_2} is : {div(num_1, num_2)}")
+
+
+
+# call the function
+Calculator()
+
+# 9th. Function with Docstring
+def calculate_area(length, width):
+    return length * width
+
+
+result= calculate_area(2,5)
+print(f"The Area of rectangle is : {result}")
 
     
-
+print(calculate_area.__doc__)
 
 
