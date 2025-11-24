ad5475bf1386b85750a9df1c74073f7565ae9c07
Gulshid Zada <gulshidzada4@gmail.com>
2025-11-24 13:25:00 +0500

    Assignemnt Completed -->

--- PYTHON/Assignment/Assignment_1.py
+++ PYTHON/Assignment/Assignment_1.py
@@ -1,24 +1,23 @@
 # Password Storage and Management System
+Upper_Case_letters = ['A' , 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-# 1.Upper and Lower Case Letters
-upper_case_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
-                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-lower_case_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+Lower_Case_letters = ['a' , 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                    'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# 2.Declare a Database
+# Create a Database of list
+
 database = []
 
-# 3. while True
-while True:
-    # Input from User at Runtime for Choices
-    print("*** Password Manager ***")
-    print("1. Add username and password")
-    print("2. Display all usernames and passwords")
-    print("3. Exit")
-    
-    choice = input("Enter the Following Option to Choose :")
+print("*** Password Manager ***\n")
+print("1. Add Username and Password")
+print("2. Display All Username and Password")
+print("3. Exit")
 
+
+while True:
+    choice = input("Enter the Following Option :)")
+    
     if choice == '1':
         username = input("Enter Your Username :")
         password = input("Enter Your Password :")
@@ -29,35 +28,35 @@ while True:
         lowerCase = 0
         
         for part in pass_parts:
-            if part in upper_case_letters:
-                upperCase +=1
-            elif part in lower_case_letters:
+            if part in Upper_Case_letters:
+                upperCase += 1
+            elif part in Lower_Case_letters:
                 lowerCase +=1
         
         if upperCase == 2 and lowerCase == 4:
             entry = username + ":" + password
             database.append(entry)
-            print("Username and Password added Successfully ---->")
+            print("USername and Password is Successfully Added --->")
         else:
             print("Invalid Password")
-            print("Upper Case Letter is :", upperCase)
-            print("Lower Case Letter is :", lowerCase)
-            print("Because Password Must have 2 Upper Case Letters and 4 Lower Case Letters")
+            print(f"UpperCase letters : {upperCase}")
+            print(f"LowerCase letters : {lowerCase}")
+            print("Becasue of Password must be 2 UpperCase and 4 LowerCase Letters --->")
+            
             
     elif choice == '2':
         if len(database) == 0:
-            print("There is no Record of any Person")
+            print("There is No Record of Student in Database :)")
         else:
-            print("*** All Users ***")    
+            print("*** ALL USers ***")
             for entry in database:
-                parts = entry.split(":")
-                print("Username :",parts[0])
-                print("Password :",parts[1]) 
-                print("- - -")
+                parts = entry.split(':')   
+                print("USername :", parts[0])
+                print("Password :", parts[1])
+                print("-- --- --- --")             
+            
     elif choice == '3':
-        
-        print("Good Bye")
+        print("Good Bye ")
         break
     else:
-        print("Invalid Input ! Please try Again")
-        
+        print("Invalid Input for Option 1, 2, 3 Exist ")
